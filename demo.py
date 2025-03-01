111ac0cd846306210e1bff68677bb86be1c9b820
Sami3160 <samiii@localhost.localdomain>
2025-03-01 10:09:05 +0530

    Add Google Dorking route and update title in index.html

--- demo.py
+++ demo.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return render_template('index.html')
+@app.route('/dorkengin')
+def dork():
+    return render_template('dorking.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
